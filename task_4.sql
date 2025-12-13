4d4475a168b945f4ccacbc428a4daec27d700c06
Damilare Adegbola <damilarejustin7@gmail.com>
2025-12-13 13:31:45 +0100

    Code fix on task 4

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,3 @@
--- SQL query to retrieve the schema of the 'books' table
--- including column names, data types, nullability, keys, defaults, and extra information
 SELECT
     TABLE_NAME,
     COLUMN_NAME,
@@ -12,8 +10,7 @@ SELECT
 FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE
-    TABLE_SCHEMA = DATABASE()
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'books'
 ORDER BY
     ORDINAL_POSITION;
-
